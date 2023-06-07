674b0bce23f0b62e5374ae7bb862f6f44db16d6d
Raj Bhattacharyya <rajbhattacharyya18110@gmail.com>
2023-06-07 12:26:23 +0530

    update

--- src/components/News.js
+++ src/components/News.js
@@ -1,73 +1,132 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 
 export default class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 5,
+    category: "general",
+  };
 
-  constructor(){
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
+  constructor() {
     super();
     this.state = {
       articles: [],
       loading: false,
-      page: 1
-  };
+      page: 1,
+    };
   }
 
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?apiKey=fc36079860474865bc569ee53665374e&country=in&pageSize=20";
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?apiKey=fc36079860474865bc569ee53665374e&category=${this.props.category}&country=${this.props.country}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
-    let parseData = await data.json()
+    let parseData = await data.json();
     console.log(parseData);
-    this.setState({articles: parseData.articles, totalResults: parseData.totalResults})
-  }
-
-  handlePrevClick = async ()=>{
-    console.log("prev")
-    let url = `https://newsapi.org/v2/top-headlines?apiKey=fc36079860474865bc569ee53665374e&country=in&page=${this.state.page - 1}&pageSize=20`;
-    let data = await fetch(url);
-    let parseData = await data.json()
     this.setState({
       articles: parseData.articles,
-      page: this.state.page - 1
-    })
+      totalResults: parseData.totalResults,
+      loading: false,
+    });
   }
-  handleNextClick = async ()=>{
-    console.log("next")
-    if(this.state.page + 1 <= Math.ceil(this.state.totalResults/20)){
-      let url = `https://newsapi.org/v2/top-headlines?apiKey=fc36079860474865bc569ee53665374e&country=in&page=${this.state.page + 1}&pageSize=20`;
-      let data = await fetch(url);
-      let parseData = await data.json()
-      this.setState({
-        articles: parseData.articles,
-        page: this.state.page + 1
-      })
-    }
-  }
+
+  handlePrevClick = async () => {
+    console.log("prev");
+    let url = `https://newsapi.org/v2/top-headlines?apiKey=fc36079860474865bc569ee53665374e&category=${
+      this.props.category
+    }&country=${this.props.country}&page=${this.state.page - 1}&pageSize=${
+      this.props.pageSize
+    }`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      articles: parseData.articles,
+      page: this.state.page - 1,
+      loading: false,
+    });
+  };
+  handleNextClick = async () => {
+    console.log("next");
+    let url = `https://newsapi.org/v2/top-headlines?apiKey=fc36079860474865bc569ee53665374e&category=${
+      this.props.category
+    }&country=${this.props.country}&page=${this.state.page + 1}&pageSize=${
+      this.props.pageSize
+    }`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      articles: parseData.articles,
+      page: this.state.page + 1,
+      loading: false,
+    });
+  };
 
   render() {
     return (
       <>
         <Container my={3}>
-      <h1 className='my-3'>NewsLetter - Top Headlines</h1>
-      <Row>
-        {this.state.articles.map((element)=>{
-          return(
-            <Col md={4} key={element.url}>
-              <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage?element.urlToImage:"https://im.indiatimes.in/content/2023/Jun/1_647ddd463c3f1.png"} newsUrl={element.url} />
-            </Col>
-          )
-        })}
-      </Row>
-      </Container>
-      <Container className='d-flex justify-content-between'>
-        <Button variant='dark' disabled={this.state.page<=1} onClick={this.handlePrevClick}> &larr; Previous</Button>
-        <Button variant='dark' onClick={this.handleNextClick} >Next &rarr; </Button>
-      </Container>
+          <h1 className="my-3 text-center">
+            NewsLetter - Top {this.props.category} headlines
+          </h1>
+          {this.state.loading && <Spinner />}
+          <Row>
+            {!this.state.loading &&
+              this.state.articles.map((element) => {
+                return (
+                  <Col md={4} key={element.url}>
+                    <NewsItem
+                      title={element.title}
+                      description={element.description}
+                      imageUrl={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://im.indiatimes.in/content/23/Jun/1_647ddd463c3f1.png"
+                      }
+                      newsUrl={element.url}
+                      author={element.author ? element.author : "anonymous"}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </Col>
+                );
+              })}
+          </Row>
+        </Container>
+        <Container className="d-flex justify-content-between">
+          <Button
+            variant="dark"
+            disabled={this.state.page <= 1}
+            onClick={this.handlePrevClick}
+          >
+            {" "}
+            &larr; Previous
+          </Button>
+          <Button
+            variant="dark"
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;{" "}
+          </Button>
+        </Container>
       </>
-      
-    )
+    );
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,26 +1,45 @@
-import React, { Component } from 'react'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
 
 export default class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
-      <div className='my-3'>
-        <Card style={{ width: '18rem' }}>
+      <div className="my-3">
+        <Card>
+          <Badge
+            pill
+            bg="warning"
+            text="dark"
+            style={{
+              position: "absolute",
+              zIndex: 1,
+              left: "75%",
+              top: "-10px",
+            }}
+          >
+            {source}
+          </Badge>
           <Card.Img variant="top" src={imageUrl} />
           <Card.Body>
             <Card.Title>{title}...</Card.Title>
+            <Card.Text>{description}...</Card.Text>
             <Card.Text>
-              {description}...
+              <small className="text-muted">
+                By {author} on {new Date(date).toGMTString()}
+              </small>
             </Card.Text>
-            <Button variant="dark" size='sm' href={newsUrl} target='_blank'>Read More</Button>
+            <Button variant="dark" size="sm" href={newsUrl} target="_blank">
+              Read More
+            </Button>
           </Card.Body>
         </Card>
       </div>
-    )
+    );
   }
 }
-
 
 //fc36079860474865bc569ee53665374e
